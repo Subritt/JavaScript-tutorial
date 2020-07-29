c5aed934c30454abd30a13c920cd238eb6a2fa7f
Subritt <subrittburlakoti@gmail.com>
2020-07-29 21:12:43 +0545

    Object and Class, and static method using ES6

--- 03Intermediate/05OOP.js
+++ 03Intermediate/05OOP.js
@@ -42,11 +42,34 @@ class Person {
     getFullName() {
         return `${this.firstName} ${this.lastName}`;
     }
+
+    getAge() {
+        const year = new Date().getFullYear() - this.dob.getFullYear();
+        return `${this.firstName + ' ' + this.lastName} is ${year} years old.`;
+    }
+
+    revise(newDOB) {
+        this.dob = new Date(newDOB);
+        this.revised = true;
+    }
+
+    /**
+     * Static method is used without instantiating any object.
+     * If you try to run it with an object it will return an error
+    */
+   static favouritePlace() {
+       return 'Switzerland';
+   }
 }
 
 // Instantiate Object
 const person1 = new Person('John', 'Doe', '4/3/1980');
 const person2 = new Person('Marry', 'Smith', '3/6/1970');
 
-console.log(person2.getFullName());
-console.log(person1);
+// console.log(person2.getFullName());
+// person1.revise('4/3/1981');
+// console.log(person1.getAge());
+
+// console.log(person1.favouritePlace()); // returns person1.favouritePlace is not a function
+
+console.log(Person.favouritePlace());
