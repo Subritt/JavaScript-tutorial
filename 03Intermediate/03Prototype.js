e31f7e6665cead8278c275e5952037930192e662
Subritt <subrittburlakoti@gmail.com>
2020-07-29 20:43:04 +0545

    prototype methods

--- 03Intermediate/03Prototype.js
+++ 03Intermediate/03Prototype.js
@@ -10,6 +10,25 @@ Book.prototype.getSummary = function () {
     return `${this.title} was written by ${this.author} in ${this.year}`;
 };
 
+// getAge
+Book.prototype.getAge = function() {
+    const years = new Date().getFullYear() - this.year;
+    return `${this.title} is ${years} old`;
+};
+
+// Revise/Change Year
+Book.prototype.revise = function (newYear) {
+    this.year = newYear;
+    this.revised = true;
+}
+
+const book1 = new Book('Book One', 'John Doe', '2013');
+const book2 = new Book('Book Two', 'Jane Doe', '2016');
+
+console.log(book2);
+book2.revise('2020');
+console.log(book2);
+
 // Magazine Constructor
 function Magazine(title, author, year, month) {
     Book.call(this, title, author, year);
@@ -26,4 +45,4 @@ const mag1 = new Magazine('Magazine One', 'John Doe', '2013', 'July');
 // Use Magazine Constructor
 Magazine.prototype.constructor = Magazine;
 
-console.log(mag1);
+// console.log(mag1);
---
+++ 03Intermediate/04ObjectCreate.js
@@ -0,0 +1,6 @@
+// Object of protos
+const bookProtos = {
+    getSummary: function () {
+        return `${this.title} was written by ${this.author} in ${this.year}`;
+    }
+}
