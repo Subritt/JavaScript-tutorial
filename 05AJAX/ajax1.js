1dc7ee33c0d9a52b73a630e93b9680f4be87ee00
Subritt <subrittburlakoti@gmail.com>
2020-07-31 21:58:30 +0545

    onload, onreadystatechange, onprogress and onerror

--- 05AJAX/ajax1.js
+++ 05AJAX/ajax1.js
@@ -8,12 +8,47 @@ function loadText() {
     // OPEN(type, url/file, async)
     xhr.open('GET', 'sample.txt', true);
 
+    console.log('READYSTATE: ', xhr.readyState);
+
+    // OPTIONAL - used for loaders
+    xhr.onprogress = function() {
+        console.log('READYSTATE: ', xhr.readyState);
+    }
+
     xhr.onload = function() {
+        console.log('READYSTATE: ', xhr.readyState); // onload won't run until readyState -> 4
         if (this.status == 200) {
-            console.log(this.responseText);
+            // console.log(this.responseText);
         }
     }
+
+    xhr.onerror = function() {
+        console.log('Request error...');
+    }
+
+    // xhr.onreadystatechange = function () {
+    //     console.log('READYSTATE: ', xhr.readyState); // checks for all readyState values
+    //     if (this.readyState == 4 && this.status == 200) {
+    //         // console.log(this.responseText);
+    //     }
+    // }
 
     // Send request
     xhr.send();
 }
+
+/**
+ * readyState Values
+ * 0: request not initialized
+ * 1: server connection established
+ * 2: request received
+ * 3: processing request
+ * 4: request finished and response is ready
+ */
+
+/**
+ * HTTP Statuses
+ * 200: "OK"
+ * 403: "Forbidden"
+ * 404: "Not Found"
+*/
