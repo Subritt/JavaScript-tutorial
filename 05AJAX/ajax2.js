fb3f5e7dd05611558e03878beb82e3cce2d4f689
Subritt <subrittburlakoti@gmail.com>
2020-08-01 21:53:12 +0545

    users.json in div

--- 05AJAX/ajax2.js
+++ 05AJAX/ajax2.js
@@ -1,5 +1,8 @@
+// Events
 document.querySelector('#btn1').addEventListener('click', loadUser);
+document.querySelector('#btn2').addEventListener('click', loadUsers);
 
+// For user.json
 function loadUser() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'user.json', true);
@@ -19,6 +22,33 @@ function loadUser() {
             `;
 
             document.querySelector('#user').innerHTML = output;
+        }
+    }
+
+    xhr.send();
+}
+
+// For users.json
+function loadUsers() {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'users.json', true);
+
+    xhr.onload = function() {
+        if(this.status == 200) {
+            const users = JSON.parse(this.responseText);
+            
+            let output = '';
+            users.forEach(user => {
+                output += `
+                    <ul>
+                        <li>ID: ${user.id}</li>
+                        <li>Name: ${user.name}</li>
+                        <li>Email: ${user.email}</li>
+                    </ul>
+                `;                
+            });
+
+            document.querySelector('#users').innerHTML = output;
         }
     }
 
