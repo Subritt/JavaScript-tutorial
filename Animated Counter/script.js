6ca06db4a19be4bc9e73564be37314a71d8a697f
Subritt <subrittburlakoti@gmail.com>
2020-08-20 21:36:47 +0545

    looping through the counter divs

--- Animated Counter/script.js
+++ Animated Counter/script.js
@@ -1,2 +1,12 @@
 const counters = document.querySelectorAll('.counter');
 const speed = 200;
+
+counters.forEach(counter => {
+    // Update Count of counters
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        console.log(target);
+    }
+
+    updateCount();
+});
